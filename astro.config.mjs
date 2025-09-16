f0089cb012ed7be77896f5e7c84d54978bae134c
jules cynamon <julescweb@gmail.com>
2025-09-16 14:04:05 +0200

    feat: ajout d'intégrations pour Tailwind, React et Sitemap
    - Intégration de Tailwind CSS pour le style
    - Ajout de React pour la gestion des composants
    - Inclusion de Sitemap pour la génération de sitemaps
    - Ces modifications améliorent la structure et les fonctionnalités du projet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://harmonia-lime.vercel.app",
   output: "server", // tout en SSR → plus de getStaticPaths obligatoire + headers OK
   adapter: vercel(),
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap(),
+  ],
 });
